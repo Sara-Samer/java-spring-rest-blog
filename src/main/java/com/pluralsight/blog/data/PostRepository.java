a6a2c25ade58b93f488f1011d2e85e8c0bce0a75
Sara Samer <ss.titanic199@gmail.com>
2022-04-26 06:24:55 +0200

    made search title containing

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -11,4 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+    
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
+}
